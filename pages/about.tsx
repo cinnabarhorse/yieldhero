793b585a6e60d1a3e703c8a5291e70f58f7e627a
Daniel M <dmathieuva@gmail.com>
2020-05-11 13:18:48 +0530

    Fixed build error

---
+++ pages/about.tsx
@@ -0,0 +1,23 @@
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+
+const AboutPage = () => (
+  <Layout>
+    <article>
+      <h1>About Yield Hero</h1>
+      <p>
+        The main idea of Yield Hero is to let you easily swap collateral between different aTokens to get the best yield.
+      </p>
+
+
+      <p>
+        Yield Hero is powered by a Balancer pool. You can view details <a href="https://kovan.pools.balancer.exchange/#/pool/0x7d3fd22FBC32FD112696E8e7cFc7Eb7f50c657b2">here.</a>
+      </p>
+
+
+
+    </article>
+  </Layout>
+)
+
+export default AboutPage
